3a36da11e9d8b036ee159371ace4550e7aabf872
Dmitrius123 <S2109011784@sd.uni-vt.bg>
2025-02-20 19:13:29 +0200

    Initial Commit

---
+++ NumbersFunTests/NumbersFunTests.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersFunTests.swift
+//  NumbersFunTests
+//
+//  Created by Дмитрий Куприянов on 20.02.25.
+//
+
+import Testing
+@testable import NumbersFun
+
+struct NumbersFunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
